53150ae873edd2556acfc0d0c648e61c7fffbe63
Enrico <enricosellanora96@gmail.com>
2020-07-28 00:37:34 +0200

    n int eccesso

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+
 function ajaxFilm(search){
   $.ajax({url: "https://api.themoviedb.org/3/search/movie?api_key=2949182a20ec9f3c52a5bedc673809f5&query=" + search,
     method: "GET",
     success: function (data) {
-      var results = data['results'];
-
+      var results = data.results;
       for (var i = 0; i < results.length; i++) {
-        /*var titolo = results[i].title;
-        var titoloOriginale = results[i].original_title;
-        var lingua = results[i].original_language;*/
-        var voto = Math.ceil(results[i].vote_average);
+        var dati = {
+          original_title : results[i].original_title,
+          title : results[i].title,
+          original_language : results[i].original_language,
+          poster_path : results[i].poster_path,
+          vote_average : Math.ceil(results[i].vote_average)
+        }
         var template = $('#template-film').html();
         var compiled = Handlebars.compile(template);
         var target = $('#risultati');
-        var filmHtml = compiled(results[i]);
+        var filmHtml = compiled(dati);
         target.append(filmHtml);
       }
   },
@@ -26,19 +29,19 @@ function ajaxSerie(search){
   $.ajax({url: "https://api.themoviedb.org/3/search/tv?api_key=2949182a20ec9f3c52a5bedc673809f5&query=" + search,
     method: "GET",
     success: function (data) {
-      var results = data['results'];
-
-      console.log(data);
+      var results = data.results;
       for (var i = 0; i < results.length; i++) {
-        /*var titolo = results[i].title;
-        var titoloOriginale = results[i].original_title;
-        var lingua = results[i].original_language;
-        var voto = parseInt(results[i].vote_average);*/
-        var voto = Math.ceil(results[i].vote_average);
+        var dati = {
+          original_name : results[i].original_name,
+          name : results[i].name,
+          original_language : results[i].original_language,
+          poster_path : results[i].poster_path,
+          vote_average : Math.ceil(results[i].vote_average)
+        }
         var template = $('#template-serie').html();
         var compiled = Handlebars.compile(template);
         var target = $('#risultati');
-        var filmHtml = compiled(results[i]);
+        var filmHtml = compiled(dati);
         target.append(filmHtml);
       }
   },
@@ -54,14 +57,14 @@ function pressInvioMsg (){
   }
 }
 
-function infoFilm(){
+function info(){
   $('.infoFilm,.infoSerie').hide();
   $('.img').show();
   $(this).hide();
   $(this).siblings('.infoFilm,.infoSerie').show();
 }
 
-function copertinaFilm(){
+function copertina(){
   $(this).hide();
   $(this).siblings('.img').show();
 }
@@ -86,7 +89,7 @@ function search(){
 function init() {
 $('#lenteRicerca').click(search);
 $('#ricerca').keyup(pressInvioMsg);
-$('#risultati').on('click','.img',infoFilm);
-$('#risultati').on('click','.infoFilm,.infoSerie',copertinaFilm);
+$('#risultati').on('click','.img',info);
+$('#risultati').on('click','.infoFilm,.infoSerie',copertina);
 }
 $(document).ready(init);
